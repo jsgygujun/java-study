0e86680dd9f977ea193f2a0abb74cd0dcfc5ba19
jsgygujun <jsgygujun@gmail.com>
2020-07-23 14:26:41 +0800

    [Init]

---
+++ example/src/main/java/com/jsgygujun/example/Hello.java
@@ -0,0 +1,11 @@
+package com.jsgygujun.example;
+
+/**
+ * @author jsgygujun@gmail.com
+ * @since 2020/7/23 2:26 下午
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
